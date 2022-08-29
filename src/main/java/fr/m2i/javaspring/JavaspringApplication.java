6d590b80574a2dee660d77177ade38bde162af06
Christian RAKOTO <crakoto@yahoo.fr>
2022-08-29 12:30:14 +0200

    On recupère le contexte et appeller le Profile

--- src/main/java/fr/m2i/javaspring/JavaspringApplication.java
+++ src/main/java/fr/m2i/javaspring/JavaspringApplication.java
@@ -1,10 +1,12 @@
 package fr.m2i.javaspring;
 
+import fr.m2i.javaspring.config.DataSourceConfig;
 import fr.m2i.javaspring.entities.Employee;
 import fr.m2i.javaspring.repository.EmployeeRepo;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
@@ -13,10 +15,14 @@ import java.util.stream.Stream;
 public class JavaspringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaspringApplication.class, args);
+
+        ApplicationContext ctx=SpringApplication.run(JavaspringApplication.class, args);
+
+        //Ici on peut appleller Config selon notre config
+        DataSourceConfig dataConfig=ctx.getBean(DataSourceConfig.class);
+        dataConfig.setup();
     }
 
 
-    //Données d'initialisation
 
 }
