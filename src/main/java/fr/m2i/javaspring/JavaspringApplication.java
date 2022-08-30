01a6543bb60dbbdeab749aad926cc8cb4f7c03ed
Christian RAKOTO <crakoto@yahoo.fr>
2022-08-30 08:26:13 +0200

    Configuration avec InternalResourceViewResolver

--- src/main/java/fr/m2i/javaspring/JavaspringApplication.java
+++ src/main/java/fr/m2i/javaspring/JavaspringApplication.java
@@ -1,15 +1,11 @@
 package fr.m2i.javaspring;
 
 import fr.m2i.javaspring.config.DataSourceConfig;
-import fr.m2i.javaspring.entities.Employee;
-import fr.m2i.javaspring.repository.EmployeeRepo;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class JavaspringApplication {
